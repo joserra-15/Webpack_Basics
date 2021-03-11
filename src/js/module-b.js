251bbe851aae116bd0e59ce90970bf9f6590eff2
joserra-15 <joseramonsr3@hotmail.com>
2021-03-11 20:37:54 +0100

    First commit

---
+++ src/js/module-b.js
@@ -0,0 +1,7 @@
+import { $dataMetaSchema } from 'ajv';
+
+/* A file called module-b.js that contains a javascript module that makes use of the JQuery library (JQuery must be imported via webpack not manually)
+ */
+export function jq() {
+  $('#app').append('<p>jquery message</p>');
+}
